182eebde09216a2f50d05f17c8eb9f42a1999962
abdiwoli <abdiwolih@gmail.com>
2024-05-19 16:36:55 +0300

    done

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" module docs """
+
+
+def index_range(page, page_size):
+    """ Write a function named index_range """
+    return ((page - 1) * page_size, page * page_size)
