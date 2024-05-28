98b8b1fd182015210927bebb09b7c297751e54a2
abdiwoli <abdiwolih@gmail.com>
2024-05-29 01:11:43 +0300

    done

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" 0-app,py """
-from flask import Flask, render_template
-from flask_babel import Babel
+""" 1-app.py """
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
 
 
 class Config:
+    """ config class """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_TIMEZONE = 'UTC'
     BABEL_DEFAULT_LOCALE = 'en'
@@ -12,16 +13,21 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
+    """ locale setup """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def home():
     """ main route """
-    return render_template('0-index.html')
+    return render_template('3-index.html')
 
-babel = Babel(app, locale_selector=get_locale)
+
 if __name__ == '__main__':
     app.run(debug=True)
